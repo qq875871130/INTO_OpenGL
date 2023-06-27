7df1fb94ef458fcbd9c50b36746667bd9eb8ed50
qq875871130 <me@cvlocke.site>
2023-06-28 00:10:29 +0800

    Complete "Hello Trangle" part and successfully draw trangle and rectangle

--- test.cpp
+++ test.cpp
@@ -2,9 +2,153 @@
 
 
 int main() {
+	return test_helloTrangle();
+}
+
+int test_helloTrangle() {
+
+#pragma region Hard-encode GLSL
+	const char* vertexShaderSource = "#version 460 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+	const char* fragmentShaderSource = "#version 460 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\0";
+#pragma endregion
+
+	//Def vertices
+	float vertices[] = {
+		//Trangle
+		//-0.5f, -0.5f, 0.0f,
+		//0.5f, -0.5f, 0.0f,
+		//0.0f,  0.5f, 0.0f
+
+		//Rectangle
+		0.5f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f
+	};
+
+	//Def indices
+	unsigned int indices[] = {
+		0,1,3,
+		1,2,3
+	};
+
+	unsigned int VBO;
+	unsigned int VAO;
+	unsigned int EBO;
+
+	//Create shader program
+	unsigned int shaderProgram;
+
+	return test_helloWindow([vertices, indices, vertexShaderSource, fragmentShaderSource, &shaderProgram, &VBO, &VAO, &EBO]() {
+		//Gen and bind VAO
+		glGenVertexArrays(1, &VAO);
+		glBindVertexArray(VAO);
+
+		//Gen and bind VBO
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		//Gen and bind EBO
+		glGenBuffers(1, &EBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		//Create vertex shader
+		unsigned int vertexShader;
+		vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+		//Append source to vertex shader and compile
+		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		glCompileShader(vertexShader);
+
+#pragma region CheckCompileAvaibility
+		int success;
+		char infoLog[512];
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+		//Print error log
+		if (!success)
+		{
+			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+				<< infoLog
+				<< std::endl;
+		}
+#pragma endregion
+
+		//Create fragment shader
+		unsigned int fragmentShader;
+		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+		//Append source to fragment shader and compile
+		glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+		glCompileShader(fragmentShader);
+
+		shaderProgram = glCreateProgram();
+
+		//Attach shaders
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		glLinkProgram(shaderProgram);
+
+#pragma region CheckProgramAvaibility
+		int success_pg;
+		char infoLog_pg[512];
+
+		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success_pg);
+		if (!success_pg) {
+			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog_pg);
+			std::cout << "ERROR::SHADER::LINK::COMPILATION_FAILED\n"
+				<< infoLog_pg
+				<< std::endl;
+		}
+#pragma endregion
+
+		//Release shaders
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+
+		//Apply vertices
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		//Clear VAO VBO
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+
+		},
+		[&shaderProgram, &VAO]() {
+			//Activate program
+			glUseProgram(shaderProgram);
+			glBindVertexArray(VAO);
+			//Draw
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+			//Unbind
+			glBindVertexArray(0);
+		},
+			[&shaderProgram, &VBO, &VAO, &EBO]() {
+			glDeleteVertexArrays(1, &VAO);
+			glDeleteBuffers(1, &VBO);
+			glDeleteBuffers(1, &EBO);
+			glDeleteProgram(shaderProgram);
+		});
+}
+
+int test_helloWindow(std::function<void()> beforeLoop, std::function<void()> onLoop, std::function<void()> afterLoop) {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	//Create Window
@@ -27,6 +171,7 @@ int main() {
 	//Set Callback
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	beforeLoop();
 
 	//Render Loop
 	while (!glfwWindowShouldClose(window))
@@ -35,13 +180,19 @@ int main() {
 		processInput(window);
 
 		//Render Command
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Render Operation
+		onLoop();
 
 		//Checking and Swap Buffer
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	afterLoop();
 
 	//Render Terminate
 	glfwTerminate();
--- test.h
+++ test.h
@@ -2,8 +2,13 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <functional>
 
 int main();
+
+int test_helloTrangle();
+
+int test_helloWindow(std::function<void()> beforeLoop, std::function<void()> onLoop, std::function<void()> afterLoop);
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
